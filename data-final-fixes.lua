d93470532db5b5e85753f2c24d482d131902552e
Stephen Faure <faure.stephen@gmail.com>
2026-02-24 17:17:25 +0100

    Remove stacking from standard drills to offer trade off between speed and stacking

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,18 +1,13 @@
 local drill_names = {
-  "electric-mining-drill",
-  "bob-mining-drill-1",
-  "bob-mining-drill-2",
-  "bob-mining-drill-3",
-  "bob-mining-drill-4",
-  "bob-area-mining-drill-1",
-  "bob-area-mining-drill-2",
-  "bob-area-mining-drill-3",
-  "bob-area-mining-drill-4",
+	"bob-area-mining-drill-1",
+	"bob-area-mining-drill-2",
+	"bob-area-mining-drill-3",
+	"bob-area-mining-drill-4",
 }
 
 for _, name in pairs(drill_names) do
-  local drill = data.raw["mining-drill"][name]
-  if drill then
-    drill.drops_full_belt_stacks = true
-  end
+	local drill = data.raw["mining-drill"][name]
+	if drill then
+		drill.drops_full_belt_stacks = true
+	end
 end
